728f5f4e6bfb61c6a4026f9f056a6cdddb3b0bb6
surestdeng <surestdeng@xiaoe-tech.com>
2020-12-02 17:52:32 +0800

    删除的差不多了

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,9 @@
 // pro components
 import Ellipsis from '@/components/Ellipsis'
 import FooterToolbar from '@/components/FooterToolbar'
-import Tree from '@/components/Tree/Tree'
-import Trend from '@/components/Trend'
 import STable from '@/components/Table'
 import MultiTab from '@/components/MultiTab'
 import IconSelector from '@/components/IconSelector'
-import TagSelect from '@/components/TagSelect'
-import StandardFormRow from '@/components/StandardFormRow'
 import ArticleListContent from '@/components/ArticleListContent'
 
 import Dialog from '@/components/Dialog'
@@ -16,15 +12,11 @@ import Dialog from '@/components/Dialog'
 export {
   RankList,
   TransferBar,
-  Trend,
   Ellipsis,
   FooterToolbar,
-  Tree,
   STable,
   MultiTab,
   IconSelector,
-  TagSelect,
-  StandardFormRow,
   ArticleListContent,
 
   Dialog
