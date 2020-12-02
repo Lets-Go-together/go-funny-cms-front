59bc9475a8b034ce2d4c881abb1ad8007f472e2b
surestdeng <surestdeng@xiaoe-tech.com>
2020-12-02 17:45:59 +0800

    继续删除

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,7 @@
-// chart
-import Bar from '@/components/Charts/Bar'
-import ChartCard from '@/components/Charts/ChartCard'
-import Liquid from '@/components/Charts/Liquid'
-import MiniArea from '@/components/Charts/MiniArea'
-import MiniSmoothArea from '@/components/Charts/MiniSmoothArea'
-import MiniBar from '@/components/Charts/MiniBar'
-import MiniProgress from '@/components/Charts/MiniProgress'
-import Radar from '@/components/Charts/Radar'
-import RankList from '@/components/Charts/RankList'
-import TransferBar from '@/components/Charts/TransferBar'
-import TagCloud from '@/components/Charts/TagCloud'
 
 // pro components
-import AvatarList from '@/components/AvatarList'
 import Ellipsis from '@/components/Ellipsis'
 import FooterToolbar from '@/components/FooterToolbar'
-import NumberInfo from '@/components/NumberInfo'
 import Tree from '@/components/Tree/Tree'
 import Trend from '@/components/Trend'
 import STable from '@/components/Table'
@@ -28,22 +14,11 @@ import ArticleListContent from '@/components/ArticleListContent'
 import Dialog from '@/components/Dialog'
 
 export {
-  AvatarList,
-  Bar,
-  ChartCard,
-  Liquid,
-  MiniArea,
-  MiniSmoothArea,
-  MiniBar,
-  MiniProgress,
-  Radar,
-  TagCloud,
   RankList,
   TransferBar,
   Trend,
   Ellipsis,
   FooterToolbar,
-  NumberInfo,
   Tree,
   STable,
   MultiTab,
