70309fce2a9c2e716d24588e19afd62b179f9a65
surest <chenf@surest.cn>
2021-05-11 21:41:52 +0800

    解决token失效下很多提示的问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,90 +7,90 @@ import { ACCESS_TOKEN } from '@/store/mutation-types'
 
 // 创建 axios 实例
 const request = axios.create({
-  // API 请求的默认前缀
-  baseURL: process.env.VUE_APP_API_BASE_URL,
-  timeout: 6000 // 请求超时时间
+    // API 请求的默认前缀
+    baseURL: process.env.VUE_APP_API_BASE_URL,
+    timeout: 6000 // 请求超时时间
 })
 
 // 异常拦截处理器
 const errorHandler = (error) => {
-  if (error.response) {
-    const data = error.response.data
-    // 从 localstorage 获取 token
-    const token = storage.get(ACCESS_TOKEN)
-    if (error.response.status === 403) {
-      notification.error({
-        message: 'Forbidden',
-        description: data.message
-      })
-      return Promise.reject(data.message)
+    if (error.response) {
+        const data = error.response.data
+            // 从 localstorage 获取 token
+        const token = storage.get(ACCESS_TOKEN)
+        if (error.response.status === 403) {
+            notification.error({
+                message: 'Forbidden',
+                description: data.message
+            })
+            return Promise.reject(data.message)
+        }
+        if (error.response.status === 401 && !(data.result && data.result.isLogin)) {
+            notification.error({
+                message: 'Unauthorized',
+                description: 'Authorization verification failed'
+            })
+            if (token) {
+                store.dispatch('Logout').then(() => {
+                    setTimeout(() => {
+                        window.location.reload()
+                    }, 1500)
+                })
+            }
+        }
     }
-    if (error.response.status === 401 && !(data.result && data.result.isLogin)) {
-      notification.error({
-        message: 'Unauthorized',
-        description: 'Authorization verification failed'
-      })
-      if (token) {
-        store.dispatch('Logout').then(() => {
-          setTimeout(() => {
-            window.location.reload()
-          }, 1500)
-        })
-      }
-    }
-  }
-  
-  notification.error({
-    message: 'Server Error',
-    description: ''
-  })
-  return Promise.reject(error)
+
+    // notification.error({
+    //   message: 'Server Error',
+    //   description: ''
+    // })
+    return Promise.reject(error)
 }
 
 // request interceptor
 request.interceptors.request.use(config => {
-  const token = storage.get(ACCESS_TOKEN)
-  // 如果 token 存在
-  // 让每个请求携带自定义 token 请根据实际情况自行修改
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`
-  }
+    const token = storage.get(ACCESS_TOKEN)
+        // 如果 token 存在
+        // 让每个请求携带自定义 token 请根据实际情况自行修改
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`
+    }
 
-  config.headers = Object.assign({
-    'Content-Type': 'application/json;charset=UTF-8'
-  }, config.headers)
+    config.headers = Object.assign({
+        'Content-Type': 'application/json;charset=UTF-8'
+    }, config.headers)
 
-  return config
+    return config
 }, errorHandler)
 
 // response interceptor
-request.interceptors.response.use(({data}) => {
-  if (data.status == 200) {
-    return data
-  }
+request.interceptors.response.use(({ data }) => {
+    if (data.status == 200) {
+        return data
+    }
 
-  console.log(data)
+    console.log(data)
 
-  let message = data.message || "系统错误，请稍后重试"
-  notification.error({
-    message: '提示',
-    description: message,
-    duration: 4
-  })
-  throw new Error(message)
-  Promise.reject(message)
+    let message = data.message || "系统错误，请稍后重试"
+    notification.error({
+        message: '提示',
+        description: message,
+        duration: 4
+    })
+    throw new Error(message)
+    Promise.reject(message)
 }, errorHandler)
 
 const installer = {
-  vm: {},
-  install (Vue) {
-    Vue.use(VueAxios, request)
-  }
+    vm: {},
+    install(Vue) {
+        Vue.use(VueAxios, request)
+    }
 }
 
 export default request
 
 export {
-  installer as VueAxios,
-  request as axios
+    installer as VueAxios,
+    request as axios
 }
